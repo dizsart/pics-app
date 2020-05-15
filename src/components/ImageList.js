ef3285e30907c468f0f5a076e5d06664c08b1138
dizsart <darogadibia@gmail.com>
2020-05-15 18:44:01 +0100

    refactor: destructure the props.images.map elements

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,11 +4,11 @@ import React from 'react';
 const ImageList = props => {
     console.log(props.images);
 
-    const images = props.images.map((image)=>{
-        return <img src ={image.urls.small}/>
+    const images = props.images.map(({id, urls, description})=>{
+        return <img key={id} src ={urls.small} alt = {description}/>
     });
 
     return <div>{images}</div>
 }
 
-export default ImageList;
+export default ImageList; 
