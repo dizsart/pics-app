2bd45bdbf787ba14370e940b9fe4dd12e522aeac
dizsart <darogadibia@gmail.com>
2020-05-16 13:35:44 +0100

    destructured some terms

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,15 @@
 import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard'
 
 
 const ImageList = props => {
-    console.log(props.images);
 
-    const images = props.images.map(({id, urls, description})=>{
-        return <img key={id} src ={urls.small} alt = {description}/>
+    const images = props.images.map((image)=>{
+        return <ImageCard key={image.id} image={image} />
     });
 
     return <div>{images}</div>
 }
 
-export default ImageList; 
+export default ImageList;  
